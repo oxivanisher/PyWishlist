2d673699eca043a68f25d57774f3e2044bdb7950
Roman Scheidegger <roman@csmarketing.ch>
2016-11-13 21:31:34 +0100

    move logic to wishes_service

--- pywishlist/blueprints/wishes/wishes.py
+++ pywishlist/blueprints/wishes/wishes.py
@@ -6,65 +6,46 @@ from flask import logging
 from flask import redirect
 from flask import request
 from flask import session
-
 from flask import url_for
 
-from pywishlist.blueprints.wishes.wishes_service import get_wish_by_id
-from pywishlist.database import db_session
+from pywishlist.blueprints.wishes.wishes_service import WishesService
 from pywishlist.login_required import login_required
-from pywishlist.user_service import get_user_by_id
-from pywishlist.utils import runQuery
 from pywishlist.models.wish import Wish
+from pywishlist.user_service import get_user_by_id
 
 log = logging.getLogger(__name__)
 
 wishes_blueprint = Blueprint('wishes_blueprint', __name__, template_folder='templates')
 
 
-@wishes_blueprint.route('/Wish/Enter', methods=['GET', 'POST'])
+@wishes_blueprint.route('/Wish/Enter', methods=['POST'])
+@login_required
+def enter_wish_post():
+    new_wish = Wish(session.get('userid'),
+                    request.form['userid'],
+                    request.form['text'])
+
+    WishesService.add_wish(new_wish)
+    return render_template('enter_wish.html')
+
+
+@wishes_blueprint.route('/Wish/Enter', methods=['GET'])
 @login_required
 def enter_wish():
-    if request.method == 'POST':
-        new_wish = Wish(session.get('userid'),
-                        request.form['userid'],
-                        request.form['text'])
-
-        db_session.add(new_wish)
-        try:
-            runQuery(db_session.commit)
-        except Exception as e:
-            log.warning("[Wish] SQL Alchemy Error on enter wish"
-                        ": %s" % e)
-            flash(gettext("The wish could not be saved"), 'error')
     return render_template('enter_wish.html')
 
 
 @wishes_blueprint.route('/Wishlists/Show/<int:user_id>', methods=['GET'])
 @login_required
 def show_wishes(user_id):
-    active_wishes = []
-    hidden_wishes = []
-    for wish in runQuery(Wish.query.all):
-        if wish.destinationId == user_id:
-            if wish.destinationId != session.get('userid'):
-                # show wishes for anothe user
-                if wish.hiddenId:
-                    hidden_wishes.append(wish)
-                else:
-                    active_wishes.append(wish)
-            elif wish.sourceId == session.get('userid'):
-                # shwo wishes for the user himself
-                if wish.hiddenId == session.get('userid'):
-                    # check if the wish was hidden by the user himself,
-                    # if it is hidden by someone else, don't show it
-                    hidden_wishes.append(wish)
-                else:
-                    active_wishes.append(wish)
+    active_wishes = WishesService.get_all_active_wishes_for_user_id(user_id)
+    hidden_wishes = WishesService.get_all_hidden_wishes_for_user_id(user_id, session.get('userid'))
 
-    if len(active_wishes) + len(hidden_wishes) == 0:
+    total_wish_count = len(active_wishes) + len(hidden_wishes)
+    if total_wish_count == 0:
         flash(gettext("No wishes found."), 'info')
 
-    log.info("Found %s wishes for user %s" % (len(active_wishes), user_id))
+    log.info("Found %s wishes for user %s" % (total_wish_count, user_id))
     return render_template('show_wishes.html',
                            wishes=active_wishes,
                            hiddenWishes=hidden_wishes,
@@ -74,21 +55,6 @@ def show_wishes(user_id):
 @wishes_blueprint.route('/Wish/Hide/<int:wish_id>/<int:user_id>', methods=['GET'])
 @login_required
 def hide_wish(wish_id, user_id):
-    wish = get_wish_by_id(wish_id)
-
-    try:
-        wish.hide(session.get('userid'))
-        db_session.merge(wish)
-        log.info("Wish %s successfully hidden by %s"
-                 % (wish.id, session.get('userid')))
-    except Exception as e:
-        flash(gettext("Unable to hide wish"), 'error')
-        log.warning("Unable to hide wish because %s" % e)
-
-    try:
-        runQuery(db_session.commit)
-    except Exception as e:
-        log.warning("[Wish] SQL Alchemy Error on hide wish"
-                    ": %s" % e)
+    WishesService.hide_wish_by_id(wish_id, hidden_by_user_id=session.get('userid'))
 
     return redirect(url_for('wishes_blueprint.show_wishes', user_id=user_id))
--- pywishlist/blueprints/wishes/wishes_service.py
+++ pywishlist/blueprints/wishes/wishes_service.py
@@ -1,6 +1,36 @@
+from pywishlist.database import db_session
 from pywishlist.models.wish import Wish
 from pywishlist.utils import runQuery
 
 
-def get_wish_by_id(wish_id):
-    return runQuery(Wish.query.filter_by(id=wish_id).first)
+class WishesService:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_wish_by_id(wish_id):
+        return runQuery(Wish.query.filter_by(id=wish_id).first)
+
+    @staticmethod
+    def hide_wish_by_id(wish_id, hidden_by_user_id):
+        wish = WishesService.get_wish_by_id(wish_id)
+        wish.hide(hidden_by_user_id)
+        runQuery(db_session.commit)
+
+    @staticmethod
+    def get_all_active_wishes_for_user_id(destination_user_id):
+        return runQuery(Wish.query.filter_by(destinationId=destination_user_id).filter_by(hiddenId=None).all)
+
+    @staticmethod
+    def get_all_hidden_wishes_for_user_id(destination_user_id, current_user_id):
+        if destination_user_id == current_user_id:
+            return runQuery(
+                Wish.query.filter_by(destinationId=destination_user_id).filter_by(hiddenId=current_user_id).all)
+        else:
+            return runQuery(
+                Wish.query.filter_by(destinationId=destination_user_id).filter(Wish.hiddenId.isnot(None)).all)
+
+    @staticmethod
+    def add_wish(new_wish):
+        db_session.add(new_wish)
+        runQuery(db_session.commit)
